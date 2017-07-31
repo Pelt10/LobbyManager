a6d58311675f663f23e2ad8917bd70b5a6827802
Pelt10 <pelt10@live.fr>
2017-07-31 22:24:45 +0200

    Update LanguageSystem

--- src/fr/pelt10/lobbymanager/Language.java
+++ src/fr/pelt10/lobbymanager/Language.java
@@ -2,54 +2,46 @@ package fr.pelt10.lobbymanager;
 
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.craftbukkit.libs.jline.internal.InputStreamReader;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Language {
-	private Plugin plugin = null;
+	private FileConfiguration config;
+	private String fileName;
+	private JavaPlugin javaPlugin;
 
-	private File configFile = null;
-	private FileConfiguration config = null;
-	private String language = null;
+    public Language(JavaPlugin javaPlugin, String language) {
+	fileName = language + ".yml";
+	this.javaPlugin = javaPlugin;
 
-	public Language(Plugin pl, String lang) {
-		plugin = pl;
-		language = lang;
-		configFile = new File(this.plugin.getDataFolder(), language + ".yml");
-		
-		this.load();
-		this.saveDefault();
+	// Load config
+	config = YamlConfiguration.loadConfiguration(new File(javaPlugin.getDataFolder(), fileName));
+
+	// Load default config
+	try (InputStream defaultConfigStream = javaPlugin.getResource(fileName)) {
+	    YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultConfigStream));
+
+	    config.setDefaults(defaultConfig);
+	    javaPlugin.saveResource(fileName, false);
+	} catch (IOException e) {
+	    throw new IllegalStateException(fileName + " not find in jar !", e);
+	}
+    }
+
+    public String getSentence(String name) {
+	String sentence = config.getString(name);
+	if (sentence == null) {
+	    javaPlugin.getLogger().log(Level.WARNING, "Missing sentence: %s", name);
+	    sentence = ChatColor.RED + "[missing sentence]";
 	}
 
-	public void load() {
-		config = YamlConfiguration.loadConfiguration(configFile);
-
-		InputStream defaultConfigStream = this.plugin.getResource(language + ".yml");
-		if (defaultConfigStream != null) {
-			@SuppressWarnings("deprecation")
-			YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(defaultConfigStream);
-			config.setDefaults(defaultConfig);
-		}
-	}
-
-	public void saveDefault() {
-		if (!configFile.exists()) {
-			plugin.saveResource(language + ".yml", false);
-		}
-	}
-
-	public String getSentence(String name) {
-		String sentence = this.config.getString(name);
-		if (sentence == null) {
-			this.plugin.getLogger().warning("Missing sentence: " + name);
-			sentence = ChatColor.RED + "[missing sentence]";
-		}
-		
-		return sentence;
-	}
-	
+	return sentence;
+    }	
 }
