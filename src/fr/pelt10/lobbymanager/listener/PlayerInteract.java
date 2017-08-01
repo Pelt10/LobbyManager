772aa8c2a294ae2b00e9fa416f45eebb44f544bf
Pelt10 <pelt10@live.fr>
2017-08-01 02:40:25 +0200

    Fix a fucking bug of Spigot ! (tempory) (SPIGOT-3481)

--- src/fr/pelt10/lobbymanager/listener/PlayerInteract.java
+++ src/fr/pelt10/lobbymanager/listener/PlayerInteract.java
@@ -1,7 +1,11 @@
 package fr.pelt10.lobbymanager.listener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,9 +18,13 @@ import fr.pelt10.lobbymanager.inventory.CustomItem;
 
 public class PlayerInteract implements Listener {
     private LobbyManager lobbyManager;
+    private Map<UUID, Location> lastPlayerLocInteract = new HashMap<>();
     
     public PlayerInteract(LobbyManager lobbyManager) {
 	this.lobbyManager = lobbyManager;
+	lobbyManager.getServer().getScheduler().runTaskTimerAsynchronously(lobbyManager, () -> {
+	    lastPlayerLocInteract.clear();
+	}, 0L, 10L);
     }
     
     @EventHandler
@@ -24,11 +32,13 @@ public class PlayerInteract implements Listener {
 	Action action = e.getAction();
 	Player player = e.getPlayer();
 	PlayerInventory playerInventory = player.getInventory();
-	
-	if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
-	    CustomItem item = lobbyManager.getInventoryManager().getHotbar()[playerInventory.getHeldItemSlot()];
-	    if (!Objects.isNull(item) && item.getItem().equals(playerInventory.getItemInMainHand())){
-		item.getItemAction().onAction(player);
+	if (!lastPlayerLocInteract.containsKey(player.getUniqueId())) {
+	    lastPlayerLocInteract.put(player.getUniqueId(), player.getLocation());
+	    if (action.equals(Action.RIGHT_CLICK_AIR) || action.equals(Action.RIGHT_CLICK_BLOCK)) {
+		CustomItem item = lobbyManager.getInventoryManager().getHotbar()[playerInventory.getHeldItemSlot()];
+		if (!Objects.isNull(item) && item.getItem().equals(playerInventory.getItemInMainHand())) {
+		    item.getItemAction().onAction(player);
+		}
 	    }
 	}
     }
